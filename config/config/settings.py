b39111f35aad3f27625b271352b9f50f685ade4d
Alexey Solovyev <solovaa@yandex.ru>
2025-11-16 17:31:32 +0000

    add secrets from .env

--- config/config/settings.py
+++ config/config/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+DOMAIN = os.getenv('DOMAIN', 'http://localhost:8000')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
